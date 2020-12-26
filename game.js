dc434d84d88eaa61916e317bf4a070026e76ed4a
thandobhebhe <bukhosi95@gmail.com>
2020-12-26 23:20:52 +0000

    refactored  playSound function for fewer lines of code

--- game.js
+++ game.js
@@ -1,47 +1,43 @@
-var gamePattern =[]
-
+var gamePattern = []
+var userClickedPattern = []
 var buttonColours = ['red', 'blue', 'green', 'yellow'];
+var level = 0;
+
+var called = false;
 
 
-function nextSequence(){
+function nextSequence(){ //random color add to the sequence
     randomNum = Math.round(Math.random()*3);
 
-    return randomNum;
-}
-
-var randomChosenColor = [nextSequence()]
-gamePattern.push(randomChosenColor);
-
-nextSequence();
-
-$('.btn').click(function(event){
-    playSound(event.target.className)
-});
-
-function playSound(name){
-    switch(name){
-        case ('btn green'): 
-        console.log('clicked '+name);
-        var greenAudio = new Audio('./sounds/green.mp3');
-        greenAudio.play();
-        break;
-    
-        case ('btn red'): 
-        console.log('clicked '+name);
-        var redAudio = new Audio('./sounds/red.mp3');
-        redAudio.play();
+    switch(randomNum){
+        case 1: 
+        gamePattern.push('red')
         break;
 
-        case ('btn blue'): 
-        console.log('clicked '+name);
-        var blueAudio = new Audio('./sounds/blue.mp3');
-        blueAudio.play();
+        case 2: 
+        gamePattern.push('blue')
         break;
 
-        case ('btn yellow'): 
-        console.log('clicked '+name);
-        var yellowfAudio = new Audio('./sounds/yellow.mp3');
-        yellowAudio.play();
+        case 3: 
+        gamePattern.push('green')
+        break;
+        case 4: 
+        gamePattern.push('yellow')
         break;
     }
+}
+
+$('.btn').click(function(event){ //get id of buttons
+    playSound(event.target.id)
+});
+
+function playSound(idName){ //play appropriate sound and animation according to the id
+
+    var userChosenColor = idName;
+    userClickedPattern.push(userChosenColor);
+    console.log(userClickedPattern)
+    console.log('GP '+gamePattern)
+
+    var sound = new Audio('./sounds/'+idName+'.mp3')
+    sound.play();
 }
